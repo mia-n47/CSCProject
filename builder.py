3c67c1a9a5fbbdb76da5d7d129d1605731e0e78c
miapn <mnelso69@calpoly.edu>
2025-02-20 10:55:56 -0800

    testing1

---
+++ builder.py
@@ -0,0 +1,22 @@
+import data
+
+database = [data.Classes("CSC 101", "TUETHU","8 am", "11am"),
+            data.Classes("PHYS 143", "TUETHU", "1 pm", "3pm"),
+            data.Classes("MATH 244", "MONTUETHUFRI", "7am", "8am"),
+            data.Classes("COMS 101", "TUETHU","12pm", "2pm"),
+            data.Classes("ENGL 145", "WEDFRI", "11am", "1pm"),
+            data.Classes("CHEM 124", "MONWED", "6pm", "9pm"),
+            data.Classes("EE 111", "FRI", "9am", "10am"),
+            data.Classes("ENGL 134", "MONWEDFRI", "4pm", "6pm"),
+            data.Classes("EE 151", "TUE", "8 am", "11 am"),
+            data.Classes("PHYS 141", "MONTUETHUFRI", "5pm", "6pm"),
+            data.Classes("MATH 241", "MONTUETHUFRI", "7am", "8am"),
+            data.Classes("MATH 142", "MONTUETHUFRI", "9am", "10am"),
+            data.Classes("MATH 141", "MONTUETHUFRI", "7am", "8am"),
+            data.Classes("ES 256", "TUETHU", "8am", "10am"),
+            ]
+
+
+def main():
+    user_input = input("Enter 6 possible classes:" )
+    possible_classes = #[[list of 4 possible classes], [list of possible 4 classes]]
